d6b84c25e94c79e1d03205d15f9fb58e91068db2
gongjian22 <1163512214@qq.com>
2022-10-13 22:48:02 +0800

    1013每日一题

---
+++ 1013/script.js
@@ -0,0 +1,12 @@
+const search = document.querySelector('.search')
+const input = document.querySelector('.input')
+const btn = document.querySelector('.btn')
+
+// search.addEventListener('click', () => {
+//   search.classList.add('active')
+// })
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active')
+  input.focus()
+})
